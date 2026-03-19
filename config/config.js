b7492812ea41d819cf2fd77f3acbcc486836cfc0
windsmasher <paczka94@gmail.com>
2026-03-19 13:23:13 +0100

    feat(master): different bank account

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ export const Config = {
   REGON: '241042730',
   KRS: 'I/645/2008',
   CompanyAddress: 'Gliwice, ul. Zwycięstwa 14/56 (II piętro)',
-  BankAccount: 'ING Bank Śląski 83 1050 1285 1000 0002 0215 1718',
+  BankAccount: '17 1050 1298 1000 0092 5145 4857',
   PhoneNumber: '+48 602 610 023',
   Email: 'mpaczka@gmail.com',
   Price: { Individual: 150, Family: 200 },
